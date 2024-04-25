5bdb5608152baa6a0049f18bc1fd146d1eb18098
Sergio Pecan <u20201a938@upc.edu.pe>
2024-04-25 08:49:29 -0500

    feat(landing-page): add new components to the landing page and login and register

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,23 @@ import {HttpClientModule} from "@angular/common/http";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import { HomeComponent } from './public/pages/home/home.component';
+import { ServicesComponent } from './public/pages/services/services.component';
+import { TeamComponent } from './public/pages/team/team.component';
+import { PricingComponent } from './public/pages/pricing/pricing.component';
+import { ContactComponent } from './public/pages/contact/contact.component';
+import { LoginComponent } from './public/pages/login/login.component';
+import { SingupComponent } from './public/pages/singup/singup.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ServicesComponent,
+    TeamComponent,
+    PricingComponent,
+    ContactComponent,
+    LoginComponent,
+    SingupComponent
   ],
   imports: [
     BrowserModule,
