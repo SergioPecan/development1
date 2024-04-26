4d4c51a02143fce096b522e78fe8423c4a1bc2cf
Sergio Pecan <u20201a938@upc.edu.pe>
2024-04-25 21:07:49 -0500

    feat: feat login user and register user component the IAM user

--- src/app/iam/pages/login/login.component.ts
+++ src/app/iam/pages/login/login.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
-
+import { UserService } from "../../service/user.service";
+import { User} from "../../entity/user.entity";
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  ruc= "";
+  password= "";
+
+  constructor(private userService: UserService) {}
+  login(){
+
+  }
 
 }
