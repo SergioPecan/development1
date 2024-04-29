70e2531c1a8dd89d8e6df8e03fd53c65ac1803b9
f4v10 <faviomsg05@gmail.com>
2024-04-28 23:23:47 -0500

    feat(employees): added employee entity and service

---
+++ src/app/resources/service/employee.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Employee} from "../model/employee.entity";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeService {
+  private apiUrl = 'http://localhost:3000/employees';
+
+  constructor(private http: HttpClient) {}
+
+  createEmployee(employee: Employee){
+    return this.http.post<Employee>(this.apiUrl, employee);
+  }
+
+
+}
