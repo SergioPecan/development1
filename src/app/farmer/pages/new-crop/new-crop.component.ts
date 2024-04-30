d6251f3f48e13623835d8bac5a766e08bb3e0e4f
Daniel Del Castillo <danielmdcb974@gmail.com>
2024-04-30 00:43:08 -0500

    feat(calendar):calendar component added, is displayed in the port

---
+++ src/app/farmer/pages/new-crop/new-crop.component.ts
@@ -0,0 +1,14 @@
+import {Component, OnInit} from '@angular/core';
+//import {FormGroup,Validators,FormBuilder} from "@angular/forms";
+
+@Component({
+  selector: 'app-new-crop',
+  templateUrl: './new-crop.component.html',
+  styleUrl: './new-crop.component.css'
+})
+export class NewCropComponent {
+
+
+  constructor() {}
+
+}
