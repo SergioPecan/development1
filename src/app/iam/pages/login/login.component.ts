fa684680dc77af1bdddb9901b6b6ac156ab8c2a3
Sergio Pecan <u20201a938@upc.edu.pe>
2024-04-30 08:00:47 -0500

    feat(iam): completed dashboard admin management

--- src/app/iam/pages/login/login.component.ts
+++ src/app/iam/pages/login/login.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-import { UserService } from "../../service/user.service";
+import { AuthService } from "../../service/auth.service";
 import { User} from "../../model/user.entity";
 import {MatSnackBar} from "@angular/material/snack-bar";
-
+import {Router} from "@angular/router";
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -12,47 +12,46 @@ export class LoginComponent {
   ruc= "";
   password= "";
 
-  constructor(private userService: UserService, private _snackBar: MatSnackBar){}
-  login(){
+
+  constructor(private userService: AuthService, private _snackBar: MatSnackBar, private router: Router){}
+// login.component.ts
+  login() {
     const user: User = {
+      IsAdmin: false,
       id: 0,
       email: '',
       password: this.password,
       phone: '',
-      ruc: this.ruc
+      ruc: this.ruc,
+      token: ''
     };
 
+    this.userService.login(user.ruc, user.password).subscribe(
+      users => {
+        if (users.length > 0) {
+          // Inicio de sesión exitoso
+          this._snackBar.open('Inicio de sesión exitoso', 'Cerrar', {
+            duration: 2000,
+            panelClass: ['login-snackbar-success']
+          });
 
-      this.userService.login(user.ruc,user.password).subscribe(
-        users => {
-          if (users.length > 0) {
-            // Inicio de sesión exitoso
-            this._snackBar.open('Inicio de sesión exitoso', 'Cerrar', {
-              duration: 2000,
-              panelClass: ['login-snackbar-success']
-            });
-
-            console.log('Inicio de sesión exitoso', users[0]);
-          } else {
-            // Inicio de sesión fallido
-            this._snackBar.open('RUC o contraseña incorrectos', 'Cerrar', {
-              duration: 20000,
-              panelClass: ['login-snackbar-error']
-            });
-            console.error('RUC o contraseña incorrectos');
-          }
-
-        },
-        error => {
-          // Aquí puedes manejar los errores
-          console.error('Hubo un error al iniciar sesión', error);
+          this.userService.loggedUser = true;
+          this.router.navigate(['/user']);
+          console.log('Inicio de sesión exitoso', users[0]);
+        } else {
+          // Inicio de sesión fallido
+          this._snackBar.open('RUC o contraseña incorrectos', 'Cerrar', {
+            duration: 20000,
+            panelClass: ['login-snackbar-error']
+          });
+          console.error('RUC o contraseña incorrectos');
         }
-      );
-
-
-
-
-
+      },
+      error => {
+        // Aquí puedes manejar los errores
+        console.error('Hubo un error al iniciar sesión', error);
+      }
+    );
   }
 
 
---
+++ src/app/iam/service/auth.service.ts
@@ -0,0 +1,96 @@
+// auth.service.ts
+// auth.service.ts
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { User } from '../model/user.entity';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { BehaviorSubject } from 'rxjs';
+import { Admin } from '../model/admin.entity';
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  loggedIn = false;
+  loggedUser = false;
+  admin: any = null; // Almacena los detalles del administrador
+  user: any = null; // Almacena los detalles del usuario
+  private apiUrl = 'http://localhost:3000/api/v1/';
+  currentAdminSubject: BehaviorSubject<Admin | null> = new BehaviorSubject<Admin | null>(null);
+  currentAdmin = this.currentAdminSubject.asObservable();
+
+  constructor(private http: HttpClient) {}
+
+  createUser(user: User) {
+    return this.http.post<User>(this.apiUrl, user);
+  }
+
+  login(ruc: string, password: string): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}usuarios?ruc=${ruc}&password=${password}`)
+      .pipe(tap(user => {
+        this.loggedUser = true;
+        this.user = user; // Almacena los detalles del usuario
+
+      }));
+  }
+
+  LoginAdmin(username: string, password: string): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}admin?username=${username}&password=${password}`)
+      .pipe(tap(admin => {
+        this.loggedIn = true;
+        this.admin = admin;
+        this.currentAdminSubject.next(admin);// Almacena los detalles del administrador
+      }));
+  }
+
+  isLoggedIn(): boolean {
+    console.log('AuthService#isLoggedIn called, returning:', this.loggedIn);
+    return this.loggedIn;
+  }
+
+  IsloggedUser(): boolean {
+    console.log('AuthService#isLoggedIn called, returning:', this.loggedUser);
+    return this.loggedUser;
+  }
+
+  getCurrentUser(): any {
+    return this.loggedUser ? this.user : null; // Devuelve los detalles del usuario actual
+  }
+
+
+
+  getCurrentAdmin():  Observable<Admin | null> {
+    return this.currentAdmin;
+  }
+  updateAdmin(admin: Admin): Observable<Admin> {
+    return this.http.put<Admin>(`${this.apiUrl}admin/${admin.id}`, admin)
+      .pipe(tap(updatedAdmin => {
+        this.admin = updatedAdmin;
+        this.currentAdminSubject.next(updatedAdmin);
+      }));
+  }
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(`${this.apiUrl}usuarios`);
+  }
+  getAdmins(): Observable<Admin[]> {
+    return this.http.get<Admin[]>(`${this.apiUrl}admins`);
+  }
+  deleteUser(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}usuarios/${id}`);
+  }
+
+
+  createAdmin(admin: Admin): Observable<Admin> {
+    return this.http.post<Admin>(`${this.apiUrl}admins`, admin);
+  }
+
+
+
+  deleteAdmin(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}admins/${id}`);
+  }
+
+
+
+}
+
