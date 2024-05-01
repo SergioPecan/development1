51347ee12be454076b5a0a6f77c9be5059f86a53
f4v10 <faviomsg05@gmail.com>
2024-04-30 19:47:56 -0500

    fix(employee.entity): fixed class Employee with new parameters and a constructor.

--- src/app/teams/model/employee.entity.ts
+++ src/app/teams/model/employee.entity.ts
@@ -1,7 +1,19 @@
-export interface Employee {
+export class Employee {
   id: number;
   firstName: string;
   lastName: string;
   dni: string;
   charge: string;
+  age: number;
+  salary: number;
+
+  constructor() {
+    this.id = 0;
+    this.firstName = "";
+    this.lastName = "";
+    this.dni = "";
+    this.charge = "";
+    this.age = 0;
+    this.salary = 0;
+  }
 }
