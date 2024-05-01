26999f12d06802e588151bbe95fc6d4fb5594b9c
f4v10 <130100844+FlowerpowerA@users.noreply.github.com>
2024-05-01 17:24:10 -0500

    feat(finance.component): added lists of collections and payments to show in html.

--- src/app/farmer/pages/finance/finance.component.ts
+++ src/app/farmer/pages/finance/finance.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import {Payment} from "../../model/payment.entity";
+import {HttpClient} from "@angular/common/http";
+import {Router} from "@angular/router";
+import {Collection} from "../../model/collection.entity";
 
 @Component({
   selector: 'app-finance',
@@ -6,5 +10,15 @@ import { Component } from '@angular/core';
   styleUrl: './finance.component.css'
 })
 export class FinanceComponent {
+  payments: Payment[] = [];
+  collections: Collection[] = []
+  constructor(private http: HttpClient, private router: Router) {
+    this.http.get<Payment[]>('http://localhost:3000/payments').subscribe(data => {
+      this.payments = data;
+    })
 
+    this.http.get<Collection[]>('http://localhost:3000/collections').subscribe(data => {
+      this.payments = data;
+    })
+  }
 }
