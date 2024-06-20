f3fd6d9186fccf646136c65efd435a4930a30046
sergio Joel Pecan Pariona <u20201a938@upc.edu.pe>
2024-06-20 07:49:06 -0500

    feat(heroku): Update server js protocols cors

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const path = require('path');
-
+const cors = require('cors')();
 const app = express();
 const createError = require('http-errors');
 // Serve static files
 app.use(express.static(path.join(__dirname, 'dist/development/browser')));
 // Send all requests to index.html
 // Send all requests to index.html
+app.use(cors);
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/development/browser/index.html'));
 });
